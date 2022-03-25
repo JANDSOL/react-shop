1fba9099c13164de6f09e84ff119850aad676022
Juan Andrade <jpas.juan01@gmail.com>
2022-03-25 18:47:04 -0500

    Toggle Menu.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/Header.scss';
+import Menu from '@components/Menu';
 import IconMenu from '@icons/icon_menu.svg';
 import LogoYardSale from '@logos/logo_yard_sale.svg';
 import IconShoppingCart from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
+	const [toggle, setToggle] = useState(false);
+
+	const handleToggle = () => {
+		setToggle(!toggle);
+	}
+
 	return (
 		<nav>
 			<img src={IconMenu} alt="menu" className="menu" />
@@ -33,13 +40,14 @@ const Header = () => {
 			</div>
 			<div className="navbar-right">
 				<ul>
-					<li className="navbar-email">platzi@example.com</li>
+					<li className="navbar-email" onClick={handleToggle}>platzi@example.com</li>
 					<li className="navbar-shopping-cart">
 						<img src={IconShoppingCart} alt="shopping cart" />
 						<div>2</div>
 					</li>
 				</ul>
 			</div>
+			{toggle && <Menu />}
 		</nav>
 	);
 }
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,10 +3,10 @@ import '@styles/ProductItem.scss';
 import BtAddToCart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
-	const [cart, setCart] = useState('');
+	const [cart, setCart] = useState([]);
 
 	const handleClick = () => {
-		setCart('Hello World!');
+		setCart([]);
 	}
 
 	return (
@@ -20,7 +20,6 @@ const ProductItem = () => {
 				<figure onClick={handleClick} >
 					<img src={BtAddToCart} alt="" />
 				</figure>
-				{cart}
 			</div>
 		</div>
 	);
