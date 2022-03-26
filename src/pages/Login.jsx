aad7b4ce313f9176a74ce49f6e56da47fe938d09
Juan Andrade <jpas.juan01@gmail.com>
2022-03-25 21:42:04 -0500

    Use Ref and Forms.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '@styles/Login.scss';
 import LogoYardSale from '@logos/logo_yard_sale.svg'
 
 const Login = () => {
+  const form = useRef(null);
+
+  const handleSubmit = (e) => {
+		e.preventDefault();
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get('email'),
+      password: formData.get('password'),
+    };
+    console.log(data);
+  }
+
 	return (
 		<div className="Login">
 			<div className="Login-container">
 				<img src={LogoYardSale} alt="logo" className="Login-Logo" />
-				<form action="/" className="Login-form">
+				<form action="/" className="Login-form" ref={form}>
 					<label htmlFor="email" className="Login-label">Email address</label>
-					<input type="text" id="email" placeholder="platzi@example.cm" className="Login-input Login-input-email" />
+					<input type="text" name="email" placeholder="platzi@example.cm" className="Login-input Login-input-email" />
 					<label htmlFor="password" className="Login-label">Password</label>
-					<input type="password" id="password" placeholder="*********" className="Login-input Login-input-password" />
-					<input type="submit" value="Log in" className="Login-primary-button Login-LoginButton" />
+					<input type="password" name="password" placeholder="*********" className="Login-input Login-input-password" />
+					<button
+						className="Login-primary-button Login-LoginButton"
+						onClick={handleSubmit}>
+						Log in
+					</button>
 					<a href="/">Forgot my password</a>
 				</form>
-				<button className="Login-secondary-button Login-signup-button">Sign up</button>
+				<button
+					className="Login-secondary-button Login-signup-button">
+					Sign up
+				</button>
 			</div>
 		</div>
 	);
